a533d6f2da6980094f6153380d4059b2d6b958fa
waldronmatt <mattwaldron@ymail.com>
2020-11-07 11:27:42 -0500

    initial netlify serverless config

--- scripts/server-prod.js
+++ scripts/server-prod.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import serverless from 'serverless-http';
 import paths from './paths';
 
 // setup
 const app = express();
 app.use(express.static(__dirname));
+app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 // config
 app.get('*', (req, res) => {
@@ -14,3 +16,5 @@ app.listen(paths.PORT, () => {
     console.log(`App listening to ${paths.PORT}....`);
     console.log('Press Ctrl+C to quit.');
 });
+
+module.exports.handler = serverless(app);
