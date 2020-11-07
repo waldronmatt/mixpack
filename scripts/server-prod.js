74aaefa5e560fff161da38bc7dd70d07549a0dd7
waldronmatt <mattwaldron@ymail.com>
2020-11-07 12:01:09 -0500

    add comments for netlify express configurations

--- scripts/server-prod.js
+++ scripts/server-prod.js
@@ -1,11 +1,26 @@
 import express from 'express';
+
+/*
+  Comment out code below for server-enabled Express without Netlify
+*/
 import serverless from 'serverless-http';
+/*
+  ------------------------------------------------------
+*/
+
 import paths from './paths';
 
 // setup
 const app = express();
 app.use(express.static(__dirname));
+
+/*
+  Comment out code below for server-enabled Express without Netlify
+*/
 app.use('/.netlify/functions/server', router);  // path must route to lambda
+/*
+  ------------------------------------------------------
+*/
 
 // config
 app.get('*', (req, res) => {
@@ -17,4 +32,10 @@ app.listen(paths.PORT, () => {
     console.log('Press Ctrl+C to quit.');
 });
 
+/*
+  Comment out code below for server-enabled Express without Netlify
+*/
 module.exports.handler = serverless(app);
+/*
+  ------------------------------------------------------
+*/
