f8ddd4e8ce5812dc873bae7cf81672ecf2f35f9e
waldronmatt <mattwaldron@ymail.com>
2021-01-23 20:38:03 -0500

    update postcss conf

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable global-require */
 module.exports = {
   plugins: {
+    // PostCSS plugin to parse CSS and add vendor prefixes to CSS rules
+    autoprefixer: {},
     // PostCSS plugin to use @import in css files
     'postcss-import': {},
     /*
@@ -8,8 +10,12 @@ module.exports = {
       Note: This plugin automatically includes Autoprefixer
     */
     'postcss-preset-env': {
-      // use the most experiemental css features
-      stage: 0,
+      /*
+        The stage option determines which CSS features to polyfill
+        stage 2 is enabled by default
+        change to a lower stage to see postcss polyfill more
+      */
+      stage: 2,
     },
   },
 };
