c152d2d96520403dd0f45be7c4ca8e3b780de494
waldronmatt <mattwaldron@ymail.com>
2021-02-01 14:31:23 -0500

    add 404 rewrite for dev server, comments

--- scripts/webpack.dev.js
+++ scripts/webpack.dev.js
@@ -31,13 +31,24 @@ module.exports = (env, argv) => {
           server.sockWrite(server.sockets, 'content-changed');
         });
       },
+      /*
+        Tells dev-server to suppress messages like the webpack bundle information.
+        Errors and warnings will still be shown.
+      */
       noInfo: true,
-      historyApiFallback: true,
-      contentBase: path.resolve(__dirname, '../dist'),
+      // use a rewrite for our 404 page to simulate express logic
+      historyApiFallback: {
+        rewrites: [{ from: /./, to: '/404.html' }],
+      },
+      // bundled files will be available in the browser under this path.
       publicPath: '/',
+      // tell the server where to serve content from (publicPath takes precedence)
+      contentBase: path.resolve(__dirname, '../dist'),
+      // open the browser after server starts
       open: true,
-      compress: true,
+      // enable webpack's Hot Module Replacement feature
       hot: true,
+      // specify a port number to listen for requests on:
       port: 8080,
       // useful for debugging
       // writeToDisk: true,
